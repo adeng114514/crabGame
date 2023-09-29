615595b9b20a4cff44ce9fd45e86ebd9e6f09654
adeng114514 <adeng25@sandomenico.org>
2023-09-29 11:00:52 -0700

    crabGame
    project.greenfoot
    Lobster.java
    Lobster.ctxt
    Lobster.class
    images
    Fruit.class
    CrabWorld.java
    CrabWorld.ctxt
    CrabWorld.class
    Crab.java
    Crab.ctxt
    Crab.class

---
+++ Crab.java
@@ -0,0 +1,28 @@
+import greenfoot.*;
+
+/**
+ * This class defines a crab. Crabs live on the beach.
+ * @author BGustin
+ * @version 6/1/2022 5:48am
+ */
+public class Crab extends Actor
+{
+    
+{
+      // Crab fields
+private String nameOfExample;   //Gives the example a name
+
+/* A private void method identified as changeNameOfExample that inputs the new name and changes the stored name to the new name.
+*/
+public void act()
+{
+     // to be implemented
+     move(-3);
+}
+}
+// Always place fields first in the class.
+    
+    // After the fields place the behaviors
+}
+
+
---
+++ CrabWorld.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
+
+public class CrabWorld extends World
+{
+    /**
+     * Create the crab world (the beach). Our world has a size 
+     * of 560x560 cells, where every cell is just 1 pixel.
+     */
+    
+    public CrabWorld() 
+    {
+        super(560, 560, 1);
+        prepareScene();
+        act();
+    }
+    
+    public void prepareScene()
+    {
+        // inst antiate new objects
+        Crab crab = new Crab();
+        
+        addObject(crab, 470, 100);
+        
+        
+    }
+}
